e6adc6ee38935642e85616616fe246b90c780db9
Alexander <sashka.rf@mail.ru>
2024-12-26 22:24:11 +0300

    main file for run all code and debug

---
+++ main.py
@@ -0,0 +1,46 @@
+from data_parser import parse_input, parse_data_section, parse_route_section, parse_order_section
+from utils import find_optimal_route
+
+
+def main():
+    file_path = 'LILA_1.txt'  # Убедитесь, что путь к файлу правильный
+
+    try:
+        # Парсинг данных
+        print("Чтение данных из файла...")
+        data_section, route_section, order_section = parse_input(file_path)
+        print("Данные успешно считаны.")
+        print("Секция DATA:", data_section)
+        print("Секция ROUTE:", route_section)
+        print("Секция ORDER:", order_section)
+
+        print("Парсинг секции DATA...")
+        data_blocks = parse_data_section(data_section)
+        print("Секция DATA разобрана:", data_blocks)
+
+        print("Парсинг секции ROUTE...")
+        route_points = parse_route_section(route_section)
+        print("Секция ROUTE разобрана:", route_points)
+
+        print("Парсинг секции ORDER...")
+        order_points = parse_order_section(order_section)
+        print("Секция ORDER разобрана:", order_section)
+
+        # Нахождение оптимального маршрута
+        print("Нахождение оптимального маршрута...")
+        optimal_path, minimal_cost = find_optimal_route(route_points, data_blocks)
+
+        # Вывод результатов
+        print("Оптимальный маршрут:")
+        for i, block in enumerate(optimal_path, 1):
+            print(f"{i}. {block}")
+        print(f"Минимальная стоимость маршрута: {minimal_cost}")
+
+    except FileNotFoundError as fnf_error:
+        print(f"Ошибка: файл не найден: {fnf_error}")
+    except ValueError as e:
+        print(f"Ошибка: {e}")
+
+
+if __name__ == "__main__":
+    main()
